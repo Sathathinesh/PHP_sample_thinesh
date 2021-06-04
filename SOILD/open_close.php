0ae77471a883ed4990ebaec5abdd508bd71da30d
S.Thinesh <44163328+Sathathinesh@users.noreply.github.com>
2021-06-04 17:40:17 +0530

    Soild

---
+++ SOILD/open_close.php
@@ -0,0 +1,62 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+interface Shape {
+    public function area();
+}
+
+class Rectangle implements Shape {
+  
+    private $width;
+    private $height;
+    
+    public function __construct($width, $height) {
+        $this->width = $width;
+        $this->height = $height;
+    }
+    
+    public function area() {
+        return $this->width * $this->height;
+    }
+}
+
+class Square implements Shape {
+  
+    private $length;
+    
+    public function __construct($length) {
+        $this->length = $length;
+    }
+    
+    public function area() {
+        return pow($this->length, 2);
+    }
+}
+
+
+class AreaCalculator {
+  
+    protected $shapes;
+    
+    public function __construct($shapes = array()) {
+        $this->shapes = $shapes;
+    }
+    
+    public function sum() {
+        $area = [];
+        
+        foreach($this->shapes as $shape) {
+            $area[] = $shape->area();
+        }
+    
+        return array_sum($area);
+    }
+}
+
+
+</body>
+</html>
