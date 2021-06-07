0d7b1195352b879593c2374707154ad1d65670a5
S.Thinesh <44163328+Sathathinesh@users.noreply.github.com>
2021-06-07 16:23:38 +0530

    cookies

---
+++ 16_sessions_cookies/02_cookies.php
@@ -0,0 +1,13 @@
+<?php
+
+// How to set cookies
+setcookie('name','TheCodehilic',time()+60 , '/');
+
+echo '<pre>';
+var_dump($_COOKIE);
+echo '<pre>';
+
+// How to update cookie
+
+// How to delete cookie
+
